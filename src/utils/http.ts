9b7757690e6a0fac8f91112caee70873ba496387
tranvankien <kientran9574@gmail.com>
2025-10-16 21:50:03 +0700

    handler error message simply from with interceptor axios

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from '../constants/httpStatusCode.enum'
+import toast from 'react-hot-toast'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,22 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    // Add a response interceptor
+    axios.interceptors.response.use(
+      function onFulfilled(response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response
+      },
+      function onRejected(error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any = error.response?.data
+          const message = data?.message || error.message
+          toast.error(message || 'Server Error')
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 const http = new Http().instance
